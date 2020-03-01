bf1268de2a6d1e8a0c8e218bddba389ad50c6554
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2020-03-01 13:19:51 +0000

    before commiting reg fiels to identity db

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegProfile.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Player1_First_Name { get; set; }
+        public string Player1_Last_Name { get; set; }
+        public string Player1_Team { get; set; }
+        public string Player2_First_Name { get; set; }
+        public string Player2_Last_Name { get; set; }
+        public string Player2_Team { get; set; }
+
+
+    }
+}
